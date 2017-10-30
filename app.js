bcb88294d2d5992b33b84c4e3a79b407efaf8185
Manan12 <manan@mokko.io>
2017-10-30 17:01:35 +0530

    Added the files and folder

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var app  = angular.module('myApp', []);
+var App  = angular.module('myApp', ['qrScanner']);
+
+App.controller('qrCrtl', ['$scope', function($scope) {
+    $scope.onSuccess = function(data) {
+        console.log(data);
+    };
+    $scope.onError = function(error) {
+        console.log(error);
+    };
+    $scope.onVideoError = function(error) {
+        console.log(error);
+    };
+}]);
